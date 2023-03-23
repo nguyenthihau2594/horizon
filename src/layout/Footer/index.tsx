16fe4b8b0692fc1a3208de70cfd7ab39a745da41
haunguyen <haunguyen@hivelab.com>
2023-03-23 15:28:05 +0700

    update style

--- src/layout/Footer/index.tsx
+++ src/layout/Footer/index.tsx
@@ -1,17 +1,24 @@
 
 import s from "./index.module.scss";
-import { Link } from "react-router-dom";
+import cx from "classnames";
 
-export default function Footer() {
+import { Link } from "react-router-dom";
+interface FooterProps{
+  className?: string;
+  sPage? : any;
+}
+
+export default function Footer({sPage, className} : FooterProps) {
+  
   return (
-    <footer className={s.footer}>
+    <footer className={cx(s.footer, sPage && className && sPage[className])}>
       <div className={s.inner}>
         <p className={s.copyright}>&copy; 2022 Horizon UI. All Rights Reserved. Made with love by Simmmple!</p>
         <ul className={s.nav__list}>
-          <li className={s.nav__item}><Link to="/admin/marketplace" className={s.nav__link}>Marketplace</Link></li>
-          <li className={s.nav__item}><Link to="/admin/license" className={s.nav__link}>License</Link></li>
-          <li className={s.nav__item}><Link to="/admin/terms" className={s.nav__link}>Terms of Use</Link></li>
-          <li className={s.nav__item}><Link to="/admin/blog" className={s.nav__link}>Blog</Link></li>
+          <li className={s.nav__item}><Link to="/admin/marketplace" className={cx(s.nav__link, sPage && className && sPage[`nav__${className}`])}>Marketplace</Link></li>
+          <li className={s.nav__item}><Link to="/admin/license" className={cx(s.nav__link, sPage && className && sPage[`nav__${className}`])}>License</Link></li>
+          <li className={s.nav__item}><Link to="/admin/terms" className={cx(s.nav__link, sPage && className && sPage[`nav__${className}`])}>Terms of Use</Link></li>
+          <li className={s.nav__item}><Link to="/admin/blog" className={cx(s.nav__link, sPage && className && sPage[`nav__${className}`])}>Blog</Link></li>
         </ul>
       </div>
     </footer>
